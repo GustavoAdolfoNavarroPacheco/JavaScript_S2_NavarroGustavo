121484aeb8534b3cc3d17ab75a53c428781379a9
GustavoAdolfoNavarroPacheco <adolfonavarro0212@gmail.com>
2025-07-31 12:44:28 -0500

    feat: :sparkles: updates

--- Dia9_HomeWork/scripts/scripts.js
+++ Dia9_HomeWork/scripts/scripts.js
@@ -7,6 +7,7 @@ function buscarPokemon() {
         if (xhr.readyState === 4 && xhr.status === 200) {
             const data = JSON.parse(xhr.responseText);
             mostrarPokemon(data);
+            mostrarSiguientePokemon(data)
         }
     };
     xhr.send();
@@ -18,31 +19,6 @@ function mostrarPokemon(datos) {
     pokemon.innerHTML = `${datos["id"]} - ${nombrePokemon}`;
     console.log(nombrePokemon)
 }
-
-
-function next() {
-    const nombreIdBuscar = document.getElementById("mainInput").value;
-    parseInt(nombreIdBuscar)
-    const nombreIdBuscar_2 = nombreIdBuscar + 1;
-    const xhr = new XMLHttpRequest();
-    const url = `https://pokeapi.co/api/v2/pokemon/${nombreIdBuscar_2}`;
-    xhr.open("GET", url, true);
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            const data = JSON.parse(xhr.responseText);
-            mostrarSiguientePokemon(data);
-        }
-    };
-    xhr.send();
-}
-
-function mostrarSiguientePokemon(datos) {
-    const pokemon = document.getElementById("mostrar");
-    const nombrePokemon = (datos["name"].charAt(0).toUpperCase()) + (datos["name"].slice(1));
-    pokemon.innerHTML = `${datos["id"]} - ${nombrePokemon}`;
-    console.log(nombrePokemon)
-}
-
 
 const input = document.getElementById("mainInput");
 input.addEventListener("keydown", function (event) {
