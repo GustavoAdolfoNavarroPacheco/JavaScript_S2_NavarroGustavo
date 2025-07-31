cf5fa41f1c62e2058bdae7aec550c6bfd7392379
GustavoAdolfoNavarroPacheco <adolfonavarro0212@gmail.com>
2025-07-30 23:42:19 -0500

    feat: :sparkles: updates

--- Dia9_HomeWork/scripts/scripts.js
+++ Dia9_HomeWork/scripts/scripts.js
@@ -6,11 +6,16 @@ function buscarPokemon() {
     xhr.open("GET", url, true);
     xhr.onreadystatechange = function () {
         if (xhr.readyState === 4 && xhr.status === 200) {
-            
-        }
+                const data = JSON.parse(xhr.responseText);
+                displayPoke(data)
+            }
     };
     xhr.send();
 }
 
+function mostrarPokemon(datos) {
+    const pokemon = document.getElementById("Mostrar");
+    const nombrePokemon = (datos["name"].chatAt(0).toUpperCase()) + (datos["name"].slice(1))
+    pokemon.innerHTML = `${datos["id"]} - ${nombrePokemon}`
+}
 
-buscarPokemon();
