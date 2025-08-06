60850b43978d83dfea9e5690598290e040a4e359
GustavoAdolfoNavarroPacheco <adolfonavarro0212@gmail.com>
2025-08-05 23:08:49 -0500

    feat: :sparkles: updates

--- Dia12+1/HomeWork/scripts/scripts.js
+++ Dia12+1/HomeWork/scripts/scripts.js
@@ -1,40 +1,44 @@
 const cors = require('cors');
+
 function buscarSuperHeroe() {
     document.getElementById("resultados").innerHTML = ``;
     const nombreUsar = document.getElementById("nombreInput").value.trim();
     console.log(nombreUsar);
     const xhr = new XMLHttpRequest();
-    const url = `https://superheroapi.com/api/d821e978ff025823536a8c91f11b3766/search/${nombreUsar}`;
+    const url = `https://superheroapi.com/api.php/d821e978ff025823536a8c91f11b3766/search/${nombreUsar}`;
     console.log(url);
     xhr.open("GET", url, true);
 
     xhr.onreadystatechange = function () {
         if (xhr.readyState === 3) {
-
-            console.log("Cargando...");
+            
+            console.log ("cargando...")
         }
         else if (xhr.readyState === 4 && xhr.status === 200) {
             try {
-                const datos = JSON.parse(xhr.responseText);
-                if (datos.results && datos.results.length > 0) {
-                    for (let i = 0; i < datos.results.length; i++) {
-                        let division = document.getElementById("resultados");
-                        division.innerHTML += `
+                const daticos = JSON.parse(xhr.responseText);
+                if (daticos.results && daticos.results.length > 0) {
+                    for (let i =0; i < daticos.results.length; i++) {
+                        let resultado = document.getElementById("resultados");
+                        resultado.innerHTML += `
                         <div class="card">
-            <img src="${datos["results"][i]["image"]}" alt="">
-            <h3>${datos["results"][i]["name"]}</h3>
-            <p><strong>Status:</strong>${datos["results"][i]["status"]}</p>
-            <p><strong>Specie:</strong>${datos["results"][i]["species"]}</p>
-        </div>
-                        `
-                        console.log(datos["results"][i]["name"]);
+                        <img src="${daticos["results"][i]["image"]["url"]}">
+                        <h3>${daticos["results"][i]["name"]}</h3>
+                        </div>`
                     }
                 }
-
             }
             catch (err) {
-                console.log(err.message);
+                console.log (err.message);
             }
         }
-    };
+    }
+    xhr.send();
 }
+
+const input = document.getElementById("nombreInput");
+input.addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+        buscarSuperHeroe();
+    }
+});
